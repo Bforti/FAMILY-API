28b729b61d37822d81f8673616c0756a3ca5fbd3
Bruno Forti <139017911+Bforti@users.noreply.github.com>
2024-06-09 18:24:26 +0000

    commit

--- src/app.py
+++ src/app.py
@@ -14,6 +14,19 @@ CORS(app)
 
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+jackson_family.add_member({    
+                        "first_name":"John",
+                        "age":33,
+                        "Lucky Numbers": [7, 13, 22]
+                        })
+jackson_family.add_member({
+                           "first_name": "Jane",
+                            "age":35,
+                           "Lucky Numbers": [10, 14, 3]})
+jackson_family.add_member({
+                            "first_name":"Jimmy" ,
+                            "age":5 ,
+                            "Lucky Numbers": [1]})
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -27,16 +40,43 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member_for_id(id):
+    member = jackson_family.get_member(id)
+    if member is None:
+        return jsonify({"msg": "No se encuentra información"}), 404
+    return jsonify(member), 200
+
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member_for_id(id):
+    success = jackson_family.delete_member(id)
+    if not success:
+        return jsonify({"msg": "No se encuentra información"}), 404
+    return jsonify({"done": True}), 200
+
+
+
+@app.route("/member",methods=['POST'])
+def new_member():
+    body=request.get_json(silent=True)
+    if body is None:
+        return jsonify({"message":"debes enviar informacion en el body"})
+    if "first_name" not in body:
+        return jsonify({"message":"el campo first_name es obligatorio"})
+    print(body)
+    new_member_data = {
+            "id": body.get("id",jackson_family._generateId()),
+            "first_name": body["first_name"],
+            "last_name": jackson_family.last_name,
+            "age": body["age"],
+            "lucky_numbers": body["lucky_numbers"]
+        }
+    return jsonify(jackson_family.add_member(new_member_data)),200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -11,6 +11,8 @@ from random import randint
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
+        self._next_id = 1
+        self._members = []
 
         # example list of members
         self._members = []
@@ -20,16 +22,27 @@ class FamilyStructure:
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+       if 'id' not in member or member['id'] is None:
+            member['id'] = self._generateId()
+       self._members.append(member)
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
-
+        for member in self._members:
+            if member['id'] == id:
+                self._members.remove(member)
+                return True
+        return False
+    def update_member(self, id, updated_member):
+        for member in self._members:
+            if member['id'] == id:
+                member.update(updated_member)
+            return True
+        return False
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if member['id'] == id:
+                return member
+        return None
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
